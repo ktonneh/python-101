25659934bc240d1ac49dff8fae541bc97376a3c9
elkibet <ktonneh@gmail.com>
2024-11-30 12:20:19 +0300

    oop

---
+++ morepractice/stringsplice.py
@@ -0,0 +1,11 @@
+story = "This is my story, this is my song"
+
+print(story)
+
+print("[0-5]",story[0:6])
+
+print('from [5]: ',story[5:])
+
+print('from 5 back : ',story[:5])
+
+print('InReverse: \n',story[::-1])

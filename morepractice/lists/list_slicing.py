0a7fe18d154aaf6790c705fd8e70996853474c67
elkibet <ktonneh@gmail.com>
2025-01-30 07:17:47 +0300

    more pracs

---
+++ morepractice/lists/list_slicing.py
@@ -0,0 +1,6 @@
+num_list = [1, 2, 3, 4, 5, 6, 7, 8]
+
+print(num_list[2:5])
+print(num_list[2:])
+print(num_list[:4])
+print(num_list[:len(num_list)])
---
+++ morepractice/lists/listindex.py
@@ -0,0 +1,7 @@
+cities = ["London", "Paris", "Los Angeles", "Beirut"]
+
+print(cities.index('London'))
+print(cities.index('Paris'))
+print(cities.index('Beirut'))
+
+
